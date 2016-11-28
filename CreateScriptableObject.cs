7ce506af7c5e2b4168ab2c8c1532cbf0f8db7dfa
iBasel <Basel.e@me.com>
2016-11-28 14:13:50 -0500

    Scripts

---
+++ CreateScriptableObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateScriptableObject : EditorWindow {
+
+    public Object source;
+
+    [MenuItem("Assets/Create Scriptable Object")]
+    public static void CreateYourScriptableObjectWindow() {
+        CreateScriptableObject window = new CreateScriptableObject();
+        window.ShowUtility();
+    }
+
+    void OnGUI()
+    {
+        source = EditorGUILayout.ObjectField(source, typeof(MonoScript), false);
+
+        if (GUILayout.Button("Create!"))
+            CreateYourScriptableObject();
+
+    }
+
+    public void CreateYourScriptableObject()
+    {
+        ScriptableObjectUtility.CreateAsset(source.name);
+    }
+}
---
+++ ScriptableObjectUtility.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class ScriptableObjectUtility
+{
+    public static void CreateAsset(string assetType)
+    {
+        ScriptableObject asset = ScriptableObject.CreateInstance(assetType);
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == string.Empty)
+        {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != string.Empty)
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), string.Empty);
+        }
+
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + assetType + ".asset");
+
+        Debug.Log("assetPathAndName " + assetType);
+
+        AssetDatabase.CreateAsset(asset, assetPathAndName);
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+}
